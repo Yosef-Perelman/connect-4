59e5061adfca91a7b1b56812e0da59fea96da802
Yosef-Perelman <yosefper@gmail.com>
2024-08-23 05:12:02 +0300

    Added circles to the board

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import GameCircle from "./GameCircle";
 import '../Game.css';
 
+const NUMBER_OF_CIRCLES = 42;
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+
 const GameBoard = () => {
+
+    const [gameBoard, setGameBoard] = useState(Array(NUMBER_OF_CIRCLES).fill(NO_PLAYER));
+    const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+
+    console.log(gameBoard);
+
+    const initBoard = () => {
+        const circles = [];
+        for (let i = 0; i < NUMBER_OF_CIRCLES; i++) {
+            circles.push(renderCircle(i));
+        }
+        return circles;
+    }
+
+    const circleClicked = (id) => {
+
+        setGameBoard(prev => {
+            return prev.map((circle, pos) => {
+                if (pos === id) return currentPlayer;
+                return circle;
+            })
+        })
+
+        setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+    }
+
+    const renderCircle = id => {
+        return <GameCircle id={id} className={`player_${gameBoard[id]}`} onCircleClicked={circleClicked}/>
+    }
+
     return <div className="gameBoard">
-        <GameCircle id={1}/>
+       {initBoard()}
     </div>
 }
 
--- src/components/GameCircle.js
+++ src/components/GameCircle.js
@@ -1,11 +1,11 @@
 import React from "react";
 import '../Game.css';
 
-const GameCircle = ({id}) => {
+const GameCircle = ({id, children, className, onCircleClicked}) => {
     console.log(id);
     return (
-        <div className="gameCircle">
-            GameCircle
+        <div className={`gameCircle ${className}`} onClick={() => onCircleClicked(id)}>
+            {children}
         </div>
     )
 }
